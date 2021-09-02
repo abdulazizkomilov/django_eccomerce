51e978c356542e8eceeb36c2a38176eedd4c6866
abulaziz <abdulazizkomilov2001@gmail.com>
2021-09-02 14:01:57 +0500

    order summary

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     HomeView, checkout,
     ItemDetailView, add_to_card,
-    remove_from_cart,
+    remove_from_cart, OrderSummaryView,
+    remove_single_item_from_cart,
 )
 
 app_name = 'core'
@@ -11,6 +12,8 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('checkout/', checkout, name='checkout'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('add-to-card/<slug>', add_to_card, name='add-to-card'),
     path('remove-from-card/<slug>', remove_from_cart, name='remove_from_cart'),
+    path('remove-item-from-cart/<slug>', remove_single_item_from_cart, name='remove-single-item-from-cart'),
 ]
