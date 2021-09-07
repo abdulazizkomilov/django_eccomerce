667e986f30d6bba90d6bdd6760d346b695545381
abulaziz <abdulazizkomilov2001@gmail.com>
2021-09-07 23:48:10 +0500

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,23 +1,27 @@
-import blank
-import null
-from django.db import models
 from django.forms import ChoiceField
+from django.db.models.signals import post_save
 from django.conf import settings
+from django.db import models
 from django.shortcuts import reverse
 from django_countries.fields import CountryField
 
-
-CATEGORY_CHOICE = (
+CATEGORY_CHOICES = (
     ('S', 'Shirt'),
     ('SW', 'Sport wear'),
-    ('OW', 'Outwear'),
+    ('OW', 'Outwear')
 )
 
-LABEL_CHOICE = (
+LABEL_CHOICES = (
     ('P', 'primary'),
     ('S', 'secondary'),
-    ('D', 'danger'),
+    ('D', 'danger')
 )
+
+ADDRESS_CHOICES = (
+    ('B', 'Billing'),
+    ('S', 'Shipping'),
+)
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(
@@ -32,11 +36,12 @@ class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
     discount_price = models.FloatField(blank=True, null=True)
-    category = models.CharField(choices=CATEGORY_CHOICE, max_length=2)
-    label = models.CharField(choices=LABEL_CHOICE, max_length=1)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
     description = models.TextField()
     quantity = models.IntegerField(default=1)
+    image = models.ImageField()
 
     def __str__(self):
         return self.title
@@ -55,6 +60,7 @@ class Item(models.Model):
         return reverse("core:remove_from_cart", kwargs={
             'slug': self.slug
         })
+
 
 class OrderItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -80,17 +86,27 @@ class OrderItem(models.Model):
             return self.get_total_discount_item_price()
         return self.get_total_item_price()
 
+
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
+    ref_code = models.CharField(max_length=20, blank=True, null=True)
     items = models.ManyToManyField(OrderItem)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
+    shipping_address = models.ForeignKey(
+        'BillingAddress', related_name='shipping_address', on_delete=models.SET_NULL, blank=True, null=True)
     billing_address = models.ForeignKey(
-        "BillingAddress", on_delete=models.SET_NULL, blank=True, null=True)
+        'BillingAddress', related_name='billing_address', on_delete=models.SET_NULL, blank=True, null=True)
     payment = models.ForeignKey(
-        "Payment", on_delete=models.SET_NULL, blank=True, null=True)
+        'Payment', on_delete=models.SET_NULL, blank=True, null=True)
+    coupon = models.ForeignKey(
+        'Coupon', on_delete=models.SET_NULL, blank=True, null=True)
+    being_delivered = models.BooleanField(default=False)
+    received = models.BooleanField(default=False)
+    refund_requested = models.BooleanField(default=False)
+    refund_granted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
@@ -99,7 +115,11 @@ class Order(models.Model):
         total = 0
         for order_item in self.items.all():
             total += order_item.get_final_price()
+        if self.coupon:
+            total -= self.coupon.amount
         return total
+
+
 
 class BillingAddress(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
