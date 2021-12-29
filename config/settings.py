71375c3ff125ef4cf57e167422d0f4843dfae0aa
abulaziz <abdulazizkomilov2001@gmail.com>
2021-12-29 07:22:24 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -161,6 +161,7 @@ AUTHENTICATION_BACKENDS = [
 
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
+
 ]
 
 SITE_ID = 1
